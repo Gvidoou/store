1fde0e3fc52975545fc9414a9e51fb641c620c0b
gvidoou@gmail.com <gvidoou@gmail.com>
2015-04-22 13:02:34 +0300

    added views

--- Products/views.py
+++ Products/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from Products.models import Product
 
-# Create your views here.
+
+class ProductList(ListView):
+    """
+    View for list of products
+    """
+    model = Product
+
+
+class ProductDetailView(DetailView):
+    """
+    View for product page
+    """
+    model = Product
