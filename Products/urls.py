8d1b604059b063a3d4c18b82a1368b35b042503b
gvidoou@gmail.com <gvidoou@gmail.com>
2015-04-22 20:56:46 +0300

    changed views and urls for posting comments

--- Products/urls.py
+++ Products/urls.py
@@ -5,6 +5,6 @@ from Products import views
 urlpatterns = patterns(
     '',
     url(r'^$', views.ProductList.as_view(), name='products_list'),
-    url(r'^(?P<slug>[-_\w]+)/$', views.ProductDetailView.as_view(), name='product'),
-
+    url(r'^(?P<slug>[-_\w]+)/$', views.ProductDetailView.as_view(),
+        name='product'),
 )
--- Products/views.py
+++ Products/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from django.core.urlresolvers import reverse
 from django.views.generic import ListView, DetailView
-from Products.models import Product
+from django.views.generic.edit import FormMixin
+from Products.forms import CommentsForm
+
+from Products.models import Product, Comments
 
 
 class ProductList(ListView):
@@ -17,15 +20,41 @@ class ProductList(ListView):
         return context
 
 
-class ProductDetailView(DetailView):
+class ProductDetailView(FormMixin, DetailView):
     """
-    View for product page
+    View for product page and form for posting comments
     """
     model = Product
-    context_object_name = 'product'
     template_name = 'product_detail.html'
+    form_class = CommentsForm
+
+    def get_success_url(self):
+        return reverse('products_list')
 
     def get_context_data(self, **kwargs):
         context = super(ProductDetailView, self).get_context_data(**kwargs)
         context['title'] = 'Product details'
+        form_class = self.get_form_class()
+        context['form'] = self.get_form(form_class)
+        try:
+            context['comments'] = Comments.objects.filter(product=self.object)
+        except:
+            pass
         return context
+
+    def post(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        print self.object
+        form_class = self.get_form_class()
+        form = self.get_form(form_class)
+        if form.is_valid():
+            data = form.cleaned_data
+            Comments.objects.create(title=data['title'],
+                                    comment=data['comment'],
+                                    product=self.object)
+            return self.form_valid(form)
+        else:
+            return self.form_invalid(form)
+
+    def form_valid(self, form):
+        return super(ProductDetailView, self).form_valid(form)
