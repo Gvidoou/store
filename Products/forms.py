58d70e8df4dfc418da179baf670a02dc9338a093
gvidoou@gmail.com <gvidoou@gmail.com>
2015-04-23 11:02:07 +0300

    added some html improvements

--- Products/forms.py
+++ Products/forms.py
@@ -6,3 +6,12 @@ class CommentsForm(ModelForm):
     class Meta:
         model = Comments
         fields = ['title', 'comment']
+
+    def __init__(self, *args, **kwargs):
+        super(CommentsForm, self).__init__(*args, **kwargs)
+        self.fields['title'].widget.attrs.update({
+            'class': 'form-control'
+        })
+        self.fields['comment'].widget.attrs.update({
+            'class': 'form-control'
+        })
